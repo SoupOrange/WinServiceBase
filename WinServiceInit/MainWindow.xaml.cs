f89591b781b0b9a4cad83eccb7560a7b1ee5c3d3
OrangeSoup <393664944@qq.com>
2021-04-25 19:09:32 +0800

    add

---
+++ WinServiceInit/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WinServiceInit
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string serviceFilePath = $"{AppDomain.CurrentDomain.BaseDirectory}\\WinServiceBase.exe";
+        string serviceName = "ServiceExample";
+
+
+
+
+        #region 服务具体操作
+        //判断服务是否存在
+        private bool IsServiceExisted(string serviceName)
+        {
+            ServiceController[] services = ServiceController.GetServices();
+            foreach (ServiceController sc in services)
+            {
+                if (sc.ServiceName.ToLower() == serviceName.ToLower())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //安装服务
+        private void InstallService(string serviceFilePath)
+        {
+            using (AssemblyInstaller installer = new AssemblyInstaller())
+            {
+                installer.UseNewContext = true;
+                installer.Path = serviceFilePath;
+                IDictionary savedState = new Hashtable();
+                installer.Install(savedState);
+                installer.Commit(savedState);
+            }
+        }
+
+        //卸载服务
+        private void UninstallService(string serviceFilePath)
+        {
+            using (AssemblyInstaller installer = new AssemblyInstaller())
+            {
+                installer.UseNewContext = true;
+                installer.Path = serviceFilePath;
+                installer.Uninstall(null);
+            }
+        }
+        //启动服务
+        private void ServiceStart(string serviceName)
+        {
+            using (ServiceController control = new ServiceController(serviceName))
+            {
+                if (control.Status == ServiceControllerStatus.Stopped)
+                {
+                    control.Start();
+                }
+            }
+        }
+
+        //停止服务
+        private void ServiceStop(string serviceName)
+        {
+            using (ServiceController control = new ServiceController(serviceName))
+            {
+                if (control.Status == ServiceControllerStatus.Running)
+                {
+                    control.Stop();
+                }
+            }
+        }
+        #endregion
+
+
+
+        private void InitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.IsServiceExisted(serviceName))
+                this.UninstallService(serviceName);
+            this.InstallService(serviceFilePath);
+        }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.IsServiceExisted(serviceName)) this.ServiceStart(serviceName);
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.IsServiceExisted(serviceName)) this.ServiceStop(serviceName);
+        }
+
+        private void UnInitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.IsServiceExisted(serviceName))
+            {
+                this.ServiceStop(serviceName);
+                this.UninstallService(serviceFilePath);
+            }
+        }
+    }
+}
